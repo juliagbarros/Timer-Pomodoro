3056a31f6ea330974f1ca514dbac71703278b428
juliagbarros <juliag1barros@gmail.com>
2026-05-04 18:26:09 -0300

    Ciclo pomodoro com pausa automática implementado

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 const tempo= document.getElementById("tempo")
 
 let tempoTotal=1500 //tempo total em segundos
-let tempoInicial=1500
+let tempoInicial=1500 //tempo total em segundos
 let progresso=0
 let timer
+let tipoSessao
+let ciclos=0
+const foco="foco"
+const pausaCurta="pausaCurta"
+const pausaLonga="pausaLonga"
 
-//ai ficar mostrando o tempo na página em minutos e segundos
+//mostra o tempo na página em minutos e segundos
 function mostrarTempo(){
-    const minutos= Math.floor(tempoTotal/60).toString().padStart(2,"0")
-    const segundos= (tempoTotal % 60).toString().padStart(2,"0")
-    tempo.innerHTML= minutos + ":"+segundos
-}
+        const minutos= Math.floor(tempoTotal/60).toString().padStart(2,"0")
+        const segundos= (tempoTotal % 60).toString().padStart(2,"0")
+        tempo.innerHTML= minutos + ":"+segundos
+
+    }
 
 //vai atualizando/diminuindo o tempo total
 //atualiza progresso da barra 
 function atualizar(){
-    if(tempoTotal>0){
-        tempoTotal--
-        mostrarTempo()
-        progresso=(tempoInicial-tempoTotal)/tempoInicial*100
-        const barraProgresso= document.getElementById("barra")
-        barraProgresso.style.width=progresso + "%"
+        if(tempoTotal>0){
+            tempoTotal--
+            mostrarTempo()
+            progresso=(tempoInicial-tempoTotal)/tempoInicial*100
+            const barraProgresso= document.getElementById("barra")
+            barraProgresso.style.width=progresso + "%"
+        }
+        else{
+            definirSessao()
+        }
     }
-    else{
-        pausar()
-    }
-}
+
 
 //vai iniciar, atualizando a cada segundo (1000 miliseg) e verifica se já foi clicado pra nao chamar mais vezes
 function iniciar(){
     if (!timer){
     timer=setInterval(atualizar,1000)
+    tipoSessao=foco
+
     }
 }
 
 //interrompe o contador e reinicia o tempo parado nos 25 minutos
 function zerar(){
-    clearInterval(timer)
+    pausar()
     tempoTotal=tempoInicial
     mostrarTempo()
-    timer=null
-
-     const barraProgresso= document.getElementById("barra")
-     progresso=0
+    const barraProgresso= document.getElementById("barra")
+    progresso=0
     barraProgresso.style.width=progresso + "%"
 
 }
@@ -51,5 +58,25 @@ function zerar(){
 function pausar(){
     clearInterval(timer)
     timer=null
+}
+
+//conta 5 minutos de pausa curta ou 15 min de pausa longa
+function definirSessao(){
+    if (tipoSessao==foco){
+        ciclos++
+
+         if(ciclos %4==0){
+            tipoSessao=pausaLonga
+            tempoInicial=900
+            tempoTotal=900
+        }
+        else{
+            tipoSessao=pausaCurta
+            tempoInicial=300
+            tempoTotal=300
+        }
+    }
+    mostrarTempo()
+
 }
 
