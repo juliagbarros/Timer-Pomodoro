cef603548f3db1826e518ce5fdc40cd75704d6ef
juliagbarros <juliag1barros@gmail.com>
2026-05-05 15:35:01 -0300

    Ciclos funcionando com mensagem e áudio de noticação

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const tempo= document.getElementById("tempo")
+const mensagem=document.getElementById("mensagem")
 
 let tempoTotal=1500 //tempo total em segundos
-let tempoInicial=1500 //tempo total em segundos
+let tempoInicial=1500 
 let progresso=0
 let timer
 let tipoSessao
@@ -30,16 +31,21 @@ function atualizar(){
         }
         else{
             definirSessao()
+            tocarAudio()
+            pausar()
         }
     }
 
 
 //vai iniciar, atualizando a cada segundo (1000 miliseg) e verifica se já foi clicado pra nao chamar mais vezes
+//verifica se é a primeira sessão, se for, exibe a mensagem de foco
 function iniciar(){
     if (!timer){
     timer=setInterval(atualizar,1000)
-    tipoSessao=foco
-
+    }
+    if (!tipoSessao){
+        tipoSessao=foco
+        mensagem.innerHTML="Hora do foco!"
     }
 }
 
@@ -69,14 +75,27 @@ function definirSessao(){
             tipoSessao=pausaLonga
             tempoInicial=900
             tempoTotal=900
+            mensagem.innerHTML= "Hora da pausa!"
+
         }
         else{
             tipoSessao=pausaCurta
             tempoInicial=300
             tempoTotal=300
+            mensagem.innerHTML= "Hora da pausa!"
+
         }
     }
+    else{
+        tipoSessao=foco
+        tempoInicial=1500
+        tempoTotal=1500
+        mensagem.innerHTML= "Hora do foco!"
+    }
     mostrarTempo()
-
 }
 
+function tocarAudio(){
+    let audio=document.getElementById("audio")
+    audio.play()
+}
